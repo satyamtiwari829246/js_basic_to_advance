4e807916394c5950d4a67456fc232ac9fe9e8809
satyamtiwari829246 <ashok8292467179@gmail.com>
2026-07-14 11:33:46 +0530

    Update javascript_object_method.js

--- javascript_object_method.js
+++ javascript_object_method.js
@@ -15,3 +15,19 @@ document.write("Name: " + person2.name + "<br>");
 document.write("Age: " + person2.age + "<br>");
 person2.Salary = 40000; //adding a new property to person2
 document.write("Salary: " + person2.Salary + "<br>");
+
+//object define Property
+let person3 = {};
+Object.defineProperty(person3, "name", {
+    value: "John Doe",
+    writable: true,
+    enumerable: true,
+    configurable: true
+});
+document.write("Name: " + person3.name + "<br>");
+person3.name = "satyam kumar"; //changing the value of name property
+document.write("Name: " + person3.name + "<br>");
+
+//getting the enteries of the object
+let result = Object.entries(person);
+document.write("Entries of the object: " + result + "<br>");
