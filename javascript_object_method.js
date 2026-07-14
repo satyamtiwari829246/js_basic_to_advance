cc4859cadbd50866a27420926a2002a7a81a98f6
satyamtiwari829246 <ashok8292467179@gmail.com>
2026-07-14 13:41:05 +0530

    Update javascript_object_method.js

--- javascript_object_method.js
+++ javascript_object_method.js
@@ -52,3 +52,37 @@ Object.defineProperty(car, "model", {
 
 let descriptor = Object.getOwnPropertyDescriptor(car, "model");
 document.write("Descriptor of model property: " + JSON.stringify(descriptor) + "<br>");
+
+let descriptor1 = Object.getOwnPropertyNames(car);
+document.write("Property names of car object: " + descriptor1 + "<br>");//it return the name of the properties of the object in an array
+
+
+let descriptor2 = Object.getOwnPropertySymbols(car);
+document.write("Property symbols of car object: " + descriptor2 + "<br>");//it return the symbol properties of the object in an array
+
+let descriptor3 = Object.getPrototypeOf(car);
+document.write("Prototype of car object: " + descriptor3 + "<br>");//it return the prototype of the object
+
+
+let descriptor4 = Object.is(car, car);
+document.write("Are car and car the same object? " + descriptor4 + "<br>");//it return true if the two objects are the same, otherwise false
+
+let descriptor5 = Object.isExtensible(car);
+document.write("Is car object extensible? " + descriptor5 + "<br>");//it return true if the object is extensible, otherwise false
+
+let descriptor6 = Object.isFrozen(car);
+document.write("Is car object frozen? " + descriptor6 + "<br>");//it return true if the object is frozen, otherwise false
+
+let descriptor7 = Object.isSealed(car);
+document.write("Is car object sealed? " + descriptor7 + "<br>");//it return true if the object is sealed, otherwise false
+
+let descriptor8 = Object.keys(car);
+document.write("Keys of car object: " + descriptor8 + "<br>");//it return the keys of the object in an array
+
+let descriptor9 = Object.values(car);
+document.write("Values of car object: " + descriptor9 + "<br>");//it return the values of the object in an array
+
+let descriptor10 = Object.entries(car);
+document.write("Entries of car object: " + descriptor10 + "<br>");//it return the entries of the object in an array
+
+object.preventExtensions(car);
