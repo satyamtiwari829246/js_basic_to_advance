cc9fafb4bf85b88f336f8400e13089989ab23dea
satyamtiwari829246 <ashok8292467179@gmail.com>
2026-07-14 11:25:13 +0530

    Update javascript_object_method.js

--- javascript_object_method.js
+++ javascript_object_method.js
@@ -5,8 +5,13 @@ let person = {
 };
 
 
-let person1 = Object.assign({}, person);
+let person1 = Object.assign({}, person);//copying the object person to person1
+let person2 = Object.create(person);//creating a new object person2 with person as its prototype
 
 document.write("Name: " + person1.name + "<br>");
 document.write("Age: " + person1.age + "<br>");
 document.write("Salary: " + person1.Salary + "<br>");
+document.write("Name: " + person2.name + "<br>");
+document.write("Age: " + person2.age + "<br>");
+person2.Salary = 40000; //adding a new property to person2
+document.write("Salary: " + person2.Salary + "<br>");
