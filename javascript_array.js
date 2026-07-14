8179e2a09ac7eaf874c34d33530f66b452634454
satyamtiwari829246 <ashok8292467179@gmail.com>
2026-07-14 13:50:40 +0530

    js array

---
+++ javascript_array.js
@@ -0,0 +1,14 @@
+var arrayname = ["satyam", "kumar", "tiwari", "priyam", "kumar", "tiwari"];
+//  document.write("Array: " + arrayname + "<br>");
+
+ var name_array = new Array("satyam", "kumar", "tiwari", "priyam", "kumar", "tiwari");;
+//  document.write("Array: " + name_array + "<br>");
+
+ var array1 = arrayname.concat(name_array);
+//  document.write("Concatenated Array: " + array1 + "<br>");
+
+var emp = new Array("jai", "vijay", "smith");
+for (var i=0; i<emp.length; i++) {
+    // document.write(emp[i] + "<br>");
+}
+
